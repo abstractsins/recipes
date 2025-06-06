588dc66ad2f5ded3d6aa3a9dbd5c744b8d44f5cd
Daniel Berlin <daniel.berlin@barcoding.com>
2025-06-06 17:13:37 -0400

    params typing

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -3,25 +3,26 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-    const id = parseInt(context.params.id)
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const id = parseInt(params.id);
 
     if (isNaN(id)) {
         return new NextResponse("Invalid user ID. Use a number.", { status: 400 });
     }
 
     try {
-        const user = await prisma.user.findUnique({
-            where: { id },
-        })
+        const user = await prisma.user.findUnique({ where: { id } });
 
         if (!user) {
             return new NextResponse("User not found", { status: 404 });
         }
 
-        return NextResponse.json({ user })
+        return NextResponse.json({ user });
     } catch (err) {
-        console.error("Error fetching user:", err)
+        console.error("Error fetching user:", err);
         return new NextResponse("Server error", { status: 500 });
     }
 }
